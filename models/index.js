c97bac6cb81d2f1831353ae0fe88302e36505432
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2022-09-28 23:50:13 -0700

    Adjusted foreign key and took out uncessary/redundant lines in index.js

--- models/index.js
+++ models/index.js
@@ -6,31 +6,22 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "product_id",
-  onDelete: "CASCADE",
+  foreignKey: "category_id",
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "product_id",
-  onDelete: "CASCADE",
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false,
-  },
-  as: "product_tags",
+  through: ProductTag,
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false,
-  },
-  as: "tag_products",
+  through: ProductTag,
 });
 
 module.exports = {
